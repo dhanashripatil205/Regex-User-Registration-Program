34e7d4e150ce556ef6a168e223486c1ebc282088
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-01-16 23:37:09 +0530

    Validate the Password by using Rule1.

--- Pattern.cs
+++ Pattern.cs
@@ -13,6 +13,8 @@ namespace Day19RejexAssignment
         public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string Reg_pattern3 = "^(abc)(.[A-Za-z]+)*@(bl).(co)([.][a-z]{2,})?$";
         public static string Reg_pattern4 = "^[0-9]{2}\\s[0-9]{10}$";
+        public static string Reg_pattern5 = "^[a-zA-Z]{8,}([0-9]+)?$";
+
 
         public static bool validateName(string name)
         {
@@ -31,6 +33,12 @@ namespace Day19RejexAssignment
         public static bool validateMobileNumber(string Monumber)
         {
             return Regex.IsMatch(Monumber, Reg_pattern4);
+
+        }
+
+        public static bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Reg_pattern5);
 
         }
 
