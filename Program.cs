ab686680e90a94e7b6eb6af51f3fa0d2e4d02cf0
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-01-16 23:16:55 +0530

    Validate Email ID

--- Program.cs
+++ Program.cs
@@ -1,13 +1,31 @@
-﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
 namespace Day19RejexAssignment
 {
-
-    class Program
+    public class Pattern
     {
-        public static void Main(string[] args)
+        public static string Reg_pattern1 = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Reg_pattern3 = "^(abc)(.[A-Za-z]+)*@(bl).(co)([.][a-z]{2,})?$";
+        public static bool validateName(string name)
         {
-            Console.WriteLine("***** Welcome To User Registration Program. ***** ");
-            
+            return Regex.IsMatch(name, Reg_pattern1);
+
         }
+        public static bool validateLastName(string lastname)
+        {
+            return Regex.IsMatch(lastname, Reg_pattern2);
+        }
+
+        public static bool validateEmailID(string email)
+        {
+            return Regex.IsMatch(email, Reg_pattern3);
+        }
+
     }
 }
