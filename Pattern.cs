60459fec53af9963c7e3430340146954736fb986
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-16 12:51:49 +0530

    As a User need to enter a valid First Name-First name starts with Cap and has minimum 3 characters

---
+++ Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19RejexAssignment
+{
+    public class Pattern
+    {
+        public static string REGEX_PAT = "^[A-Z]{1}[a-zA-Z0-9]{2,20}$";
+        public static bool validatePincode(string name)
+        {
+            return Regex.IsMatch(name, REGEX_PAT);
+        }
+    }
+}
