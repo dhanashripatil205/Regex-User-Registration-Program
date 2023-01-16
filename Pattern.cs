ec838fbfc5ac5e2800e9275a51f17021e594ebf7
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-01-16 23:27:03 +0530

    Validate Mobile Number.

--- Pattern.cs
+++ Pattern.cs
@@ -12,6 +12,8 @@ namespace Day19RejexAssignment
         public static string Reg_pattern1 = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string Reg_pattern3 = "^(abc)(.[A-Za-z]+)*@(bl).(co)([.][a-z]{2,})?$";
+        public static string Reg_pattern4 = "^[0-9]{2}\\s[0-9]{10}$";
+
         public static bool validateName(string name)
         {
             return Regex.IsMatch(name, Reg_pattern1);
@@ -25,6 +27,11 @@ namespace Day19RejexAssignment
         public static bool validateEmailID(string email)
         {
             return Regex.IsMatch(email, Reg_pattern3);
+        }
+        public static bool validateMobileNumber(string Monumber)
+        {
+            return Regex.IsMatch(Monumber, Reg_pattern4);
+
         }
 
     }
