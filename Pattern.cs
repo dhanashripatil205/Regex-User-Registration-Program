bd6cb41229afa603fa7f5f578746bfefc98caed7
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-01-17 15:11:14 +0530

    Updated Pattern.cs

--- Pattern.cs
+++ Pattern.cs
@@ -14,6 +14,8 @@ namespace Day19RejexAssignment
         public static string Reg_pattern3 = "^(abc)(.[A-Za-z]+)*@(bl).(co)([.][a-z]{2,})?$";
         public static string Reg_pattern4 = "^[0-9]{2}\\s[0-9]{10}$";
         public static string Reg_pattern5 = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}([0-9]+)?$";
+        public static string Reg_pattern6 = "^[A-Z]{1,}[a-zA-Z]{7,}([0-9]+)?$";
+        public static string Reg_pattern7 = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}([0-9]+)?$";
 
 
         public static bool validateName(string name)
@@ -39,6 +41,18 @@ namespace Day19RejexAssignment
         public static bool validatePassword(string password)
         {
             return Regex.IsMatch(password, Reg_pattern5);
+
+        }
+        
+         public static bool validatePassword1(string password)
+        {
+            return Regex.IsMatch(password, Reg_pattern6);
+
+        }
+
+        public static bool validatePassword2(string password)
+        {
+            return Regex.IsMatch(password, Reg_pattern7);
 
         }
 
